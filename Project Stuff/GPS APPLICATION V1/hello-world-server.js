4690182e85179d43bc7f75038b3be93934487f40
mnofsing <nofs.email@gmail.com>
2019-11-07 19:09:47 -0500

    Added GPS APP V1 contains a Node.js project. LOOK AT THE README

---
+++ Project Stuff/GPS APPLICATION V1/hello-world-server.js
@@ -0,0 +1,17 @@
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./MyTempFile.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(1337, '127.0.0.1');
+});
+
+console.log('Server running at http://127.0.0.1:1337/');
+
---
+++ Project Stuff/README.txt
@@ -0,0 +1,10 @@
+go to 
+
+https://www.dev2qa.com/node-js-hello-world-example-with-eclipse/
+
+AND
+
+https://www.dev2qa.com/how-to-install-node-js-in-windows/
+
+to configure Node.js in your eclispe workspace.
+
