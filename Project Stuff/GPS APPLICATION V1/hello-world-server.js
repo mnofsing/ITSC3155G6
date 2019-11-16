4148288dd6049366c2cc0402cf207c0d5fa7eb94
mnofsing <nofs.email@gmail.com>
2019-11-16 14:16:41 -0500

    Add functionality linking the login page to the main page. Also changed the server to use Express.js

--- Project Stuff/GPS APPLICATION V1/hello-world-server.js
+++ Project Stuff/GPS APPLICATION V1/hello-world-server.js
@@ -1,17 +1,25 @@
-var http = require('http'),
-    fs = require('fs');
+// Require express and create an instance of it
+var express = require('express');
+var app = express();
 
-
-fs.readFile('./MyTempFile.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(1337, '127.0.0.1');
+// on the request to root (localhost:3000/)
+app.get('/', function (req, res) {
+    res.sendfile('\login.html');
 });
 
-console.log('Server running at http://127.0.0.1:1337/');
+// On localhost:3000/welcome
+app.get('/MainPage', function (req, res) {
+    res.sendfile('\MainPage.html');
+});
+
+// Change the 404 message modifing the middleware
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
+});
+
+// start the server in the port 3000 !
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000.');
+});
+
 
