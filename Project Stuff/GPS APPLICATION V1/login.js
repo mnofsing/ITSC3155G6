49d7b812b9b9797cd6f5a3fe6c00b2b5a5b1fcba
Rohan Sitapara <rohansitapara3@gmail.com>
2019-11-17 19:40:31 -0500

    fixed error  message bug

--- Project Stuff/GPS APPLICATION V1/login.js
+++ Project Stuff/GPS APPLICATION V1/login.js
@@ -8,15 +8,18 @@
     const userName = document.getElementById("userName").value;
     const password = document.getElementById("password").value;
     console.log(userName,password);
+    let detailCorrect = false;
     for (let [key, value] of Object.entries(data)) {
         if(userName === key && password === value){
             sessionStorage.setItem('loginTime', new Date().getTime());
-            console.log(window.location.hostname,window.location.pathname);
-            const pathname = window.location.pathname;
+            //set flag true to detect user entered id and password is correct
+            detailCorrect = true;
            window.location.href = 'MainPage.html';
-        } else {
-            document.getElementById('error').classList.remove('hide');
-        }
+        } 
+    }
+
+    if(!detailCorrect) {
+        document.getElementById('error').classList.remove('hide');
     }
 }
 
