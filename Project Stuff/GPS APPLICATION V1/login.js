432f8c259a223d155b2af622ed58ae9ae5dee8b4
Rohan Sitapara <rohansitapara3@gmail.com>
2019-12-08 12:59:47 -0500

    Fixed minor errors

--- Project Stuff/GPS APPLICATION V1/login.js
+++ Project Stuff/GPS APPLICATION V1/login.js
@@ -1,33 +1,33 @@
- /*** creates session when user clicks login button
-  * params null
-  * returns void
-  */
- function createSession (e) {
+/*** creates session when user clicks login button
+ * params null
+ * returns void
+ */
+function createSession(e) {
     e.preventDefault();
-    if(document.getElementById('option2').checked){
+    if (document.getElementById('option2').checked) {
         createManagerSession(e);
     }
-    else{
+    else {
         createDriverSession(e);
     }
- }
+}
 
- function createDriverSession(e) {
+function createDriverSession(e) {
     e.preventDefault();
     const userName = document.getElementById("userName").value;
     const password = document.getElementById("password").value;
-    console.log(userName,password);
+    console.log(userName, password);
     let detailCorrect = false;
     for (let [key, value] of Object.entries(drivers)) {
-        if(userName === key && password === value){
+        if (userName === key && password === value) {
             sessionStorage.setItem('loginTime', new Date().getTime());
             //set flag true to detect user entered id and password is correct
             detailCorrect = true;
-            window.location.href = 'page1.html';
-        } 
+            window.location.href = 'driverPage.html';
+        }
     }
 
-    if(!detailCorrect) {
+    if (!detailCorrect) {
         document.getElementById('error').classList.remove('hide');
     }
 }
@@ -36,29 +36,29 @@ function createManagerSession(e) {
     e.preventDefault();
     const userName = document.getElementById("userName").value;
     const password = document.getElementById("password").value;
-    console.log(userName,password);
+    console.log(userName, password);
     let detailCorrect = false;
     for (let [key, value] of Object.entries(managers)) {
-        if(userName === key && password === value){
+        if (userName === key && password === value) {
             sessionStorage.setItem('loginTime', new Date().getTime());
             //set flag true to detect user entered id and password is correct
             detailCorrect = true;
             window.location.href = 'managerPage.html';
-        } 
+        }
     }
 
-    if(!detailCorrect) {
+    if (!detailCorrect) {
         document.getElementById('error').classList.remove('hide');
     }
 }
 
 const drivers = {
-    'jsmith' : 'pass',
-    'rsanchez' : 'pass',
-    'hdriveman' : 'pass',
+    'jsmith': 'pass',
+    'rsanchez': 'pass',
+    'hdriveman': 'pass',
 }
 
 const managers = {
-    'kjordan' : 'pass',
-    'rmathews' : 'pass'
+    'kjordan': 'pass',
+    'rmathews': 'pass'
 }
